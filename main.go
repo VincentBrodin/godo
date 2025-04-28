c765b1d31624c0d7c3915009872a02befcb217f6
Vincent Brodin <vincent.brodin21@gmail.com>
2025-04-28 22:34:09 +0200

    Moved cli base to cobra

--- main.go
+++ main.go
@@ -7,45 +7,65 @@ import (
 	"github.com/VincentBrodin/godo/pkg/engine"
 	"github.com/VincentBrodin/godo/pkg/parser"
 	"github.com/VincentBrodin/godo/pkg/utils"
+	"github.com/spf13/cobra"
 )
 
 func main() {
-	// Start of by reading the file
-	data, err := utils.ReadByName("godo", ".exe", ".exe~", ".dll", ".so", ".dylib", ".test", ".out")
+	var rootCmd = &cobra.Command{
+		Use:   "godo [task]",
+		Short: "Godo is a task runner",
+		Args:  cobra.ArbitraryArgs,
+		Run: func(cmd *cobra.Command, args []string) {
+			godoFile, err := loadFile()
+			if err != nil {
+				return
+			}
+
+			if len(args) == 0 {
+				listCommands(godoFile)
+				return
+			}
+
+			command, ok := godoFile.Commands[args[0]]
+			if !ok {
+				fmt.Printf("%s is not a command in godo file\n", args[0])
+				return
+			}
+
+			if err := engine.Run(command); err != nil {
+				fmt.Println(err)
+			}
+		},
+	}
+
+	if err := rootCmd.Execute(); err != nil {
+		fmt.Println(err)
+		os.Exit(1)
+	}
+}
+
+func loadFile() (*parser.GodoFile, error) {
+	file, err := utils.ReadByName("godo", ".exe", ".exe~", ".dll", ".so", ".dylib", ".test", ".out")
 	if err != nil {
 		fmt.Println("No godo file found!")
-		return
+		return nil, err
 	}
 
-	godoFile, err := parser.Parse(data)
+	godoFile, err := parser.Parse(file)
 	if err != nil {
 		fmt.Printf("Could not parse godo file: %s", err)
-		return
+		return nil, err
 	}
 
-	args := os.Args[1:]
-	argc := len(args)
+	return godoFile, nil
+}
 
-	// No args
-	if argc <= 0 {
-		for name, command := range godoFile.Commands {
-			if command.Description != nil {
-				fmt.Printf("%s:\n  -  %s\n", name, *command.Description)
-			} else {
-				fmt.Printf("%s\n", name)
-			}
+func listCommands(godoFile *parser.GodoFile) {
+	for name, command := range godoFile.Commands {
+		if command.Description != nil {
+			fmt.Printf("%s:\n  -  %s\n", name, *command.Description)
+		} else {
+			fmt.Printf("%s\n", name)
 		}
-		return
-	}
-
-	// Select command
-	command, ok := godoFile.Commands[args[0]]
-	if !ok {
-		fmt.Printf("%s is not a command in godo file\n", args[0])
-		return
-	}
-
-	if err := engine.Run(command); err != nil {
-		fmt.Println(err)
 	}
 }
